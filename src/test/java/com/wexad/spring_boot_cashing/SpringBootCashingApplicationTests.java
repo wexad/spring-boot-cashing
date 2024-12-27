e8bd24916c84eec24b137a8347e17518048a4f2d
wexad <sherzodchoriyev747@gmail.com>
2024-12-27 15:40:02 +0500

    initial commit

---
+++ src/test/java/com/wexad/spring_boot_cashing/SpringBootCashingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wexad.spring_boot_cashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCashingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
